2ee2d961d6c6f3f8cf626f6a429a9088a1af0431
gvidon <gvidon@ottofeller.com>
2016-11-06 21:48:38 +0300

    Wrap different helpers into recompose.compose(), add connect() by default

--- {{cookiecutter.component_name}}/index.js
+++ {{cookiecutter.component_name}}/index.js
@@ -8,14 +8,20 @@ import {connect} from 'react-redux';
 {%- if cookiecutter.has_routes == "True" %}export {default as routes} from './routes';
 {% endif -%}
 import React{% if cookiecutter.add_flow != "True" %}, {PropTypes}{% endif %} from 'react';
+import {compose} from 'recompose';
 
 {%- if cookiecutter.add_flow == "True" %}
+
+const
+	{{cookiecutter.component_name}} = ({}: Props) => <div>
+	</div>;
 
 type Props = {
 };
 
-export default connect()(({}: Props) => <div>
-</div>);
+export default compose(
+	connect()
+)({{cookiecutter.component_name}});
 {% else %}
 
 const
@@ -25,5 +31,7 @@ const
 {{cookiecutter.component_name}}.propTypes = {
 };
 
-export default connect()({{cookiecutter.component_name}})
+export default compose(
+	connect()
+)({{cookiecutter.component_name}});
 {% endif -%}
