0e3e5e82a8e1587340141b976320b92046c96d42
gvidon <gvidon@ottofeller.com>
2017-02-28 17:42:53 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 setup(
 	name='cookiecutter-reacto-component',
 	packages=[],
-	version='0.4.0',
+	version='0.4.1',
 	description='Cookiecutter template for reacto component',
 	author='Artem Rudenko',
 	author_email='gvidon@ottofeller.com',
